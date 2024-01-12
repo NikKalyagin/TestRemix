162875a8db28198e428c0987f17b70a969842607
NikKalyagin <nik.kalyagin@gmail.com>
2024-01-12 22:00:17 +0500

    hello

---
+++ Nik_MD_project1.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract ENS {
+    struct DomainInfo {
+        address userAddress;
+        uint creationTime;
+        uint fee;
+        string message;
+    }
+
+    mapping(string => DomainInfo) public   domain;
+    
+    address public userAddress;
+    uint public fees = 1000000;
+
+    function registerDomain(string memory _input) public payable  {
+        domain[_input] = DomainInfo({
+            userAddress: msg.sender,
+            creationTime: block.timestamp,
+            fee: fees,
+            message: _input
+        });
+        
+    }
+
+    function returnAddress(string memory _input) public view returns(string memory){
+        return domain[_input].message;
+    }
+
+    
+    function withdrawFunds() public {
+        address payable receiver = payable(msg.sender);
+        receiver.transfer(address(this).balance);
+    }
+}
