ddc9ff4bd53364ca78b734d2f3c615b530ea4a80
NikKalyagin <nik.kalyagin@gmail.com>
2024-01-17 15:33:56 +0500

    7

---
+++ test1.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract EventDemo { 
+  mapping(address => uint) public tokenBalance; 
+  event TokensSent(address _from, address _to, uint _amount); 
+
+  constructor() { 
+    tokenBalance[msg.sender] = 10000000000; 
+  } 
+         
+  function sendToken(address _to, uint _amount) public  { 
+    tokenBalance[msg.sender] -= _amount; 
+    tokenBalance[_to] += _amount; 
+         
+    emit TokensSent(msg.sender, _to, _amount);
+  } 
+}
